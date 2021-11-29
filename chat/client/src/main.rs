787086c092198831d1e6166fd5b02215ab985112
iTrofa <luismn2000@hotmail.com>
2021-11-29 12:01:00 +0100

    Rust Chat Client Server

--- chat/client/src/main.rs
+++ chat/client/src/main.rs
@@ -1,53 +1,288 @@
+//client
+
 use std::io::{self, ErrorKind, Read, Write};
 use std::net::TcpStream;
 use std::sync::mpsc::{self, TryRecvError};
 use std::thread;
 use std::time::Duration;
+extern crate colored;
+use colored::*;
+extern crate magic_crypt;
+use magic_crypt::MagicCryptTrait;
+//use magic_crypt::new_magic_crypt;
+
+
+// client
 
 const LOCAL: &str = "127.0.0.1:6000";
-const MSG_SIZE: usize = 32;
+const MSG_SIZE: usize = 128;
+
+pub fn add(a: i32, b:i32) -> i32{
+    a+b
+}
+pub fn is_server_online(ip: &str) -> bool{
+    match TcpStream::connect(ip){
+        Ok(_) => {return true},
+        Err(_) => {return false}
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super ::*;
+
+    #[test]
+    fn test_add(){
+        assert_eq!(add(1,2),3);
+    }
+    #[test]
+    fn test_if_server_online(){
+        assert_eq!(is_server_online(LOCAL), true);
+    }
+}
+fn try_again(){
+    println!("{}", "Appuyer sur :\n1 pour vous inscrire\n2 pour vous connecter".red().bold());
+}
+fn remove_trailing_zeros(data: &mut Vec<u8>) -> Vec<u8> {
+    // Used to remove the zeros at the end of the received encrypted message
+    // but not inside the message (purpose of the 'keep_push' var
+
+    let mut transit:Vec<u8> = vec![];
+    let mut res:Vec<u8> = vec![];
+    let mut keep_push = false;
+    for d in data.iter().rev() {
+        if *d == 0 && !keep_push{
+            continue;
+        } else {
+            transit.push(*d);
+            keep_push = true;
+        }
+    }
+    for t in transit.iter().rev() {
+        res.push(*t);
+    }
+    return res.to_owned();
+}
 
 fn main() {
-    let mut client = TcpStream::connect(LOCAL).expect("Stream failed to connect");
-    client.set_nonblocking(true).expect("failed to initiate non-blocking");
+    if is_server_online(LOCAL){
+        let mut client = TcpStream::connect(LOCAL).expect("Stream failed to connect");
+        // let mut client_send = client.try_clone().expect("clone failed..");
+        client.set_nonblocking(true).expect("failed to initiate non-blocking");
+        let client_adrr = client.local_addr();
+        let (tx, rx) = mpsc::channel::<String>();
+        let mut authenticated = false;
+        let mut conn = false;
+        let mut count = 0;
+        let mut _username = String::new();
+        thread::spawn(move || loop {
+            let mut buff = vec![0; MSG_SIZE];
+            match client.read_exact(&mut buff) {
+                Ok(_) => {
+                    if authenticated && count > 1{
+                        let msg = buff.into_iter().take_while(|&x| x != 0).collect::<Vec<_>>();
+                        let msg = String::from_utf8(msg).expect("Invalid utf8 message");
+                        let now = chrono::Utc::now();
+                        let mc = magic_crypt::new_magic_crypt!("cledeouf", 256);
+                        println!("[{}] - {:?}", now.format("%b %-d, %-I:%M").to_string().green().bold(), mc.decrypt_base64_to_string(msg).unwrap());
+                    }else{
+                        if count < 1{
+                            buff = remove_trailing_zeros(&mut buff);
+                            let mc = magic_crypt::new_magic_crypt!("cledeouf", 256);
+                            let client_val = mc.decrypt_base64_to_string(std::str::from_utf8(&buff).unwrap());
+                            match client_val{
+                                Ok(_) => {
+                                    if mc.decrypt_base64_to_string(std::str::from_utf8(&buff).unwrap()).unwrap() == "true" {
+                                        println!("Verifying login!");
+                                        conn = true;
+                                        authenticated = true;
+                                        count = 2;
+                                        println!("{}", "Authenticated ! You may now chat below !".blue().bold());
+                                    }else if mc.decrypt_base64_to_string(std::str::from_utf8(&buff).unwrap()).unwrap() == "false"{
+                                        println!("Verifying login!");
+                                        count = -1;
+                                        conn = true;
+                                        println!("still here");
+                                    }
+                                },
+                                Err(_) => {}
+                            }
+                            
+                            if !conn{
+                                println!("Authenticating with server!");
+                                buff = remove_trailing_zeros(&mut buff);
+                                let mc = magic_crypt::new_magic_crypt!("cledeouf", 256);
+                                println!("{:?}", mc.decrypt_base64_to_string(std::str::from_utf8(&buff).unwrap()).unwrap());             
+                            }
+                        }else{
+                            if !conn{
+                                let msg = buff.into_iter().take_while(|&x| x != 0).collect::<Vec<_>>();
+                                let msg = String::from_utf8(msg).expect("Invalid utf8 message");
+                                let mc = magic_crypt::new_magic_crypt!("cledeouf", 256);
+                                _username = mc.decrypt_base64_to_string(msg).unwrap();
+                                // println!("{:?}", _username);
+                                authenticated = true;
+                                println!("{}", "Authenticated ! You may now chat below !".blue().bold());
+                            }
 
-    let (tx, rx) = mpsc::channel::<String>();
+                        }
+        
+                        count = count + 1;
+                    }
+                },
+                Err(ref err) if err.kind() == ErrorKind::WouldBlock => (),
+                Err(_) => {
+                    println!("la connection au server est coupée");
+                    break;
+                }
+            }
 
-    thread::spawn(move || loop {
-        let mut buff = vec![0; MSG_SIZE];
-        match client.read_exact(&mut buff) {
-            Ok(_) => {
-                let msg = buff.into_iter().take_while(|&x| x != 0).collect::<Vec<_>>();
-                println!("message recv {:?}", msg);
-            },
-            Err(ref err) if err.kind() == ErrorKind::WouldBlock => (),
-            Err(_) => {
-                println!("connection with server was severed");
-                break;
+            match rx.try_recv() {
+                Ok(msg) => {
+                    let mut buff = msg.clone().into_bytes();
+                    buff.resize(MSG_SIZE, 0);
+                    client.write_all(&buff).expect("writing to socket failed");
+                }, 
+                Err(TryRecvError::Empty) => (),
+                Err(TryRecvError::Disconnected) => {}
+            }
+            
+            thread::sleep(Duration::from_millis(100));
+        });
+        
+        // Identification
+
+        struct Identification{
+            user: String,
+            password: String,
+            choice: String,
+        }
+
+        let mut input_choice = String::new();
+        let mut input_user = String::new();
+        let mut input_password = String::new();
+        println!("
+
+        _____ _           _     ____            
+       / ____| |         | |   |  _ \\           
+      | |    | |__   __ _| |_  | |_) | _____  __
+      | |    | '_ \\ / _` | __| |  _ < / _ \\ \\/ /
+      | |____| | | | (_| | |_  | |_) | (_) >  < 
+       \\_____|_| |_|\\__,_|\\__| |____/ \\___/_/\\_\\
+   
+       ");
+
+        loop{
+            println!("{}", "\nBienvenue sur le Chat. Souhaitez-vous vous inscrire ou vous connecter ?\n\n1) S'inscrire\n\n2) Se connecter\n\n".blue().bold());
+            match io::stdin().read_line(& mut input_choice){
+                Ok(1) => {
+                    if input_choice=="2\n"{
+                        break;
+                    }
+                    if input_user.len() > 1{
+                        // println!("{}", input_choice);
+                        break;
+                    }
+                    try_again();
+                    input_choice=String::new();
+                }
+                Ok(2) => {
+                    // println!("{}", input_choice);
+                    if input_choice=="1\n"{
+                        break;
+                    }
+                    if input_choice=="2\n"{
+                        break;
+                    }
+                    try_again();
+                    input_choice=String::new();
+                }
+                Ok(_) => {
+                    try_again();
+                    input_choice=String::new();
+                }
+                Err(e) => println!("oups {}", e)
+            }
+        }
+        loop{
+            println!("\nVeuillez entrer votre nom d'utilisateur");
+            match io::stdin().read_line(& mut input_user){
+                Ok(_) => {
+                    if input_user.len() > 1{
+                        break;
+                    }
+                    input_user.clear();
+                }
+                Err(e) => println!("oups {}", e)
             }
         }
 
-        match rx.try_recv() {
-            Ok(msg) => {
-                let mut buff = msg.clone().into_bytes();
-                buff.resize(MSG_SIZE, 0);
-                client.write_all(&buff).expect("writing to socket failed");
-                println!("message sent {:?}", msg);
-            }, 
-            Err(TryRecvError::Empty) => (),
-            Err(TryRecvError::Disconnected) => break
+        loop{
+            println!("\nVeuillez entrer votre mot de passe");
+            match io::stdin().read_line(& mut input_password){
+                Ok(_) => {
+                    if input_password.len() > 1{
+                        break;
+                    }
+                    input_password.clear();
+                }
+                Err(e) => println!("oups {}", e)
+            }
+        }
+        let info = Identification {
+            user: String::from(input_user),
+            password: String::from(input_password),
+            choice: String::from(input_choice),
+        };
+
+        let array: [String; 2] = [String::from(info.choice.to_string()+"\n\t\n"+&info.user.to_string()+"\n\t\n"+&info.password.to_string()),String::from(":quit")];
+            
+        loop {
+            let mut buff2;
+            for x in array {
+                buff2 = x.to_string();
+                let msg = buff2.trim().to_string();
+                if msg == ":quit" || tx.send(msg).is_err() {break}
+            }
+            break;
         }
 
-        thread::sleep(Duration::from_millis(100));
-    });
+        //
+        // Lancement du programme si retour ok du serveur
+        let connection = sqlite::open("../server/rust.db").unwrap();
 
-    println!("Write a Message:");
-    loop {
-        let mut buff = String::new();
-        io::stdin().read_line(&mut buff).expect("reading from stdin failed");
-        let msg = buff.trim().to_string();
-        if msg == ":quit" || tx.send(msg).is_err() {break}
+
+        loop {
+            let mut buff = String::new();
+            io::stdin().read_line(&mut buff).expect("reading from stdin failed");
+            let msg = buff.trim().to_string();
+            let mut msg2 = msg.clone();
+            let mut usernamefinal = String::new();
+            // println!("{:?}", client_adrr);
+            let mut sqlcommande = String::from("SELECT client_id FROM EN_LIGNE where ip_port = '");
+            sqlcommande.push_str(&client_adrr.as_ref().unwrap().to_string());
+            sqlcommande.push_str("';");
+            // println!("{}", sqlcommande);
+            connection
+            .iterate(sqlcommande, |pairs| {
+                for &(_column, value) in pairs.iter() {
+                    // println!("Ton username : {}", value.unwrap());
+                    usernamefinal = value.unwrap().to_string();
+                }
+                true
+            })
+            .unwrap();    
+            // println!("{:?}", _username);
+            msg2 = usernamefinal + &" : " + &msg2;
+            let mc = magic_crypt::new_magic_crypt!("cledeouf", 256);
+            let base64_msg = mc.encrypt_str_to_base64(msg2);
+
+            //println!("{}", msg);
+            if msg == ":quit" || tx.send(base64_msg).is_err() {break}
+        }
+        println!("bye bye!");
+    }else{
+        println!("{}", "\n\tLe serveur n'est pas allumé!\n".red().bold());
     }
-    println!("bye bye!");
-
 }
+
--- chat/server/src/main.rs
+++ chat/server/src/main.rs
@@ -1,42 +1,244 @@
+//server
 use std::io::{ErrorKind, Read, Write};
 use std::net::TcpListener;
+use std::net::TcpStream;
 use std::sync::mpsc;
 use std::thread;
+extern crate sqlite;
+extern crate magic_crypt;
+extern crate colored;
+extern crate rand;
+
+use magic_crypt::MagicCryptTrait;
+use colored::*;
+use rand::Rng; // 0.8.0
 
 const LOCAL: &str = "127.0.0.1:6000";
-const MSG_SIZE: usize = 32;
+const MSG_SIZE: usize = 128;
+
+// server
+fn send_message_to_client(client: &mut TcpStream, msg: &[u8]) {
+    // Used to send to a specific client
+    let mut buff = msg.to_vec();
+    buff.resize(MSG_SIZE, 0);
+    client.write_all(&buff).map(|_| client).ok();
+    // println!("I'm out");
+}
 
 fn sleep() {
     thread::sleep(::std::time::Duration::from_millis(100));
 }
 
 fn main() {
+
     let server = TcpListener::bind(LOCAL).expect("Listener failed to bind");
     server.set_nonblocking(true).expect("failed to initialize non-blocking");
-
+    let mut premiereconnection = true;
     let mut clients = vec![];
     let (tx, rx) = mpsc::channel::<String>();
+    let mut number = 1;
+    let connection = sqlite::open("rust.db").unwrap();
+    connection
+    .execute(
+        "
+        DROP TABLE IF EXISTS EN_LIGNE;
+        CREATE TABLE IF NOT EXISTS EN_LIGNE (client_id TEXT, ip_port TEXT, username TEXT, passwd TEXT);
+        ",
+    )
+    .unwrap();
+    
     loop {
+
         if let Ok((mut socket, addr)) = server.accept() {
             println!("Client {} connected", addr);
+            // println!("{}", number);  
+            // creation de la commande pour insertion des nouvelles connections dans la base de donnée EN_LIGNE
+            let addresse_ip = &addr.to_string();
+            // println!("{}", addresse_ip);
+            let mut sqlcommande = String::from("INSERT INTO EN_LIGNE VALUES (");
+            sqlcommande.push_str("'Client ");
+            sqlcommande.push_str(&number.to_string());
+            sqlcommande.push_str("','");
+            sqlcommande.push_str(&addresse_ip.to_string());
+            sqlcommande.push_str("','");
+            sqlcommande.push_str("','");
+            sqlcommande.push_str("');");
 
+            // println!("{}", sqlcommande);
+            number+=1;
+            
+            connection
+                .execute(
+                    sqlcommande
+                    ,
+                )
+                .unwrap();
+
+            connection
+            .iterate("SELECT COUNT(*) FROM EN_LIGNE", |pairs| {
+                for &(_column, value) in pairs.iter() {
+                    println!("Il y a actuellement {} utilisateur(s) en ligne", value.unwrap());
+                }
+                true
+            })
+            .unwrap();            
+                        
             let tx = tx.clone();
             clients.push(socket.try_clone().expect("failed to clone client"));
-
             thread::spawn(move || loop {
                 let mut buff = vec![0; MSG_SIZE];
-
                 match socket.read_exact(&mut buff) {
                     Ok(_) => {
                         let msg = buff.into_iter().take_while(|&x| x != 0).collect::<Vec<_>>();
-                        let msg = String::from_utf8(msg).expect("Invalid utf8 message");
+                        // Checker en base de données et integrer les informations
+                        if premiereconnection==true{
+                            let mut vecteurmsg = Vec::new();
+                            let mut tableau_info = Vec::new();
+                            let msg2 = msg.clone();
+                            for x in msg2{
+                                let min:u8 = 10;
+                                let mun:u8 = 9;
+                                if x!=min && x!=mun{
+                                    vecteurmsg.push(x)
+                                }else{
+                                    tableau_info.push(vecteurmsg);
+                                    vecteurmsg = Vec::new();
+                                }
+                            }
+                            tableau_info.push(vecteurmsg);
+                            let user = &tableau_info[4];
+                            let user = String::from_utf8(user.to_vec());
+                            let choice = &tableau_info[0];
+                            let choice = String::from_utf8(choice.to_vec());
+                            let passwd = &tableau_info[8];
+                            let passwd = String::from_utf8(passwd.to_vec());
+                            println!("User : {} - Password : {} - Mode : {}",user.as_ref().unwrap(), passwd.as_ref().unwrap(), choice.as_ref().unwrap());
+                            let connection = sqlite::open("rust.db").unwrap();
+                            if choice.as_ref().unwrap() == &"1".to_string(){
+                                // Generate random number in the range [0, 99]
+                                let mut randnum = String::new(); 
+                                for _ in 1..5 {
+                                    let num = rand::thread_rng().gen_range(0..10);
+                                    randnum = randnum.to_string() + &num.to_string();
+                                }   
+                                //BDD
+                                // on ajoute le pseudo avec l'id unique.
 
-                        println!("{}: {:?}", addr, msg);
-                        tx.send(msg).expect("failed to send msg to rx");
+                                let mut sqlcommande = String::from("UPDATE EN_LIGNE SET client_id = '");
+                                sqlcommande.push_str(&user.as_ref().unwrap().to_string());
+                                sqlcommande.push_str("#");
+                                sqlcommande.push_str(&randnum.to_string());
+                                sqlcommande.push_str("' WHERE ip_port = '");
+                                sqlcommande.push_str(&addr.to_string());
+                                sqlcommande.push_str("';");
+                                connection.execute(sqlcommande,).unwrap();   
+
+                                // on ajoute le pseudo utilisé pour login 
+
+                                let mut sqlcommande = String::from("UPDATE EN_LIGNE SET username = '");
+                                sqlcommande.push_str(user.as_ref().unwrap());
+                                sqlcommande.push_str("' WHERE ip_port = '");
+                                sqlcommande.push_str(&addr.to_string());
+                                sqlcommande.push_str("';");
+                                connection.execute(sqlcommande,).unwrap(); 
+
+                                // on ajoute le mdp du client
+
+                                let mut sqlcommande = String::from("UPDATE EN_LIGNE SET passwd = '");
+                                sqlcommande.push_str(passwd.as_ref().unwrap());
+                                sqlcommande.push_str("' WHERE ip_port = '");
+                                sqlcommande.push_str(&addr.to_string());
+                                sqlcommande.push_str("';");
+                                connection.execute(sqlcommande,).unwrap(); 
+                                    let userrand = user.as_ref().unwrap().to_owned() + &"#" + &randnum;
+                                    let message_bienvenue = "Bienvenue sur le chat!";
+                                    let mc = magic_crypt::new_magic_crypt!("cledeouf", 256);
+                                    let base64_msg = mc.encrypt_str_to_base64(message_bienvenue);
+                                    let client_username = mc.encrypt_str_to_base64(userrand);
+                                    // on renvoie l'user au client pour utiliser dans le chat.
+                                    send_message_to_client(& mut socket, base64_msg.as_bytes());
+                                    send_message_to_client(& mut socket, client_username.as_bytes());
+                            }else{
+                                // println!("CONNECTION");
+                                let mut sqlcommande = String::from("SELECT count(*) FROM EN_LIGNE WHERE username = '");
+                                sqlcommande.push_str(user.as_ref().unwrap());
+                                sqlcommande.push_str("' and passwd = '");
+                                sqlcommande.push_str(passwd.as_ref().unwrap());
+                                sqlcommande.push_str("';");
+                                // println!("{}", sqlcommande);
+                                let mut validate = "false";
+                                connection.iterate(sqlcommande, |pairs| {
+                                    for &(_column, value) in pairs.iter() {
+                                        if value.unwrap() == "1"{
+                                            validate = "true";
+                                        }
+                                    }
+                                    true
+                                }).unwrap();
+                                let mc = magic_crypt::new_magic_crypt!("cledeouf", 256);
+                                let client_validate = mc.encrypt_str_to_base64(validate);
+                                send_message_to_client(& mut socket, client_validate.as_bytes());
+
+                                if validate == "true"{
+                                    // we recover the client_id from the other connection
+                                    let mut sqlcommande = String::from("SELECT client_id FROM EN_LIGNE WHERE username = '");
+                                    sqlcommande.push_str(user.as_ref().unwrap());
+                                    sqlcommande.push_str("' and passwd = '");
+                                    sqlcommande.push_str(passwd.as_ref().unwrap());
+                                    sqlcommande.push_str("';");
+                                    let mut conn_client_id = String::new();
+                                    connection.iterate(sqlcommande, |pairs| {
+                                        for &(_column, value) in pairs.iter() {
+                                            if value.unwrap().len() > 1{ 
+                                                conn_client_id = value.unwrap().to_string();
+                                            }
+                                        }
+                                        true
+                                    }).unwrap();
+                                    // add client_id to new connection 
+                                    let mut sqlcommande = String::from("UPDATE EN_LIGNE SET client_id = '");
+                                    sqlcommande.push_str(&conn_client_id);
+                                    sqlcommande.push_str("' WHERE ip_port = '");
+                                    sqlcommande.push_str(&addr.to_string());
+                                    sqlcommande.push_str("';");
+                                    connection.execute(sqlcommande,).unwrap();
+                                }
+                             
+                            }
+                        }
+                        
+                        if premiereconnection==false{
+                            let msg = String::from_utf8(msg).expect("Invalid utf8 message");
+                            // ici ajouter le user de la bdd via jointure en ligne -- user
+                            tx.send(msg).expect("failed to send msg to rx");
+                            
+                            
+                        }
+                        premiereconnection=false;
                     }, 
                     Err(ref err) if err.kind() == ErrorKind::WouldBlock => (),
                     Err(_) => {
                         println!("closing connection with: {}", addr);
+                        // Supprimer ici les mecs de la bdd EN_LIGNE
+                        let mut sqlcommande = String::from("DELETE FROM EN_LIGNE WHERE ip_port = '");
+                        sqlcommande.push_str(&addr.to_string());
+                        sqlcommande.push_str("';");
+                        // println!("{}", sqlcommande);
+                        let connection = sqlite::open("rust.db").unwrap();
+                        connection
+                        .execute(
+                            sqlcommande
+                            ,
+                        )
+                        .unwrap();
+                        connection
+                        .iterate("SELECT COUNT(*) FROM EN_LIGNE", |pairs| {
+                            for &(_column, value) in pairs.iter() {
+                                println!("Il y a actuellement {} utilisateur(s) en ligne", value.unwrap());
+                            }
+                            true
+                        })
+                        .unwrap();   
                         break;
                     }
                 }
@@ -49,11 +251,12 @@ fn main() {
             clients = clients.into_iter().filter_map(|mut client| {
                 let mut buff = msg.clone().into_bytes();
                 buff.resize(MSG_SIZE, 0);
-
                 client.write_all(&buff).map(|_| client).ok()
             }).collect::<Vec<_>>();
+        let mc = magic_crypt::new_magic_crypt!("cledeouf", 256);
+        let now = chrono::Utc::now();
+        println!("[{}] - {:?}", now.format("%b %-d, %-I:%M").to_string().green().bold(), mc.decrypt_base64_to_string(&msg).unwrap());
         }
-
         sleep();
     }
 }
